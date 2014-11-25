2877971c5fa29440e6dfde6511acd64f70d45a33
Dave Reed <dave256@mac.com>
2014-11-25 10:01:52 -0500

    Initial Commit

---
+++ FetchedResultsTableViewController/FetchedResultsTableViewController.h
@@ -0,0 +1,19 @@
+//
+//  FetchedResultsTableViewController.h
+//  FetchedResultsTableViewController
+//
+//  Created by David M Reed on 11/25/14.
+//  Copyright (c) 2014 David M Reed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FetchedResultsTableViewController.
+FOUNDATION_EXPORT double FetchedResultsTableViewControllerVersionNumber;
+
+//! Project version string for FetchedResultsTableViewController.
+FOUNDATION_EXPORT const unsigned char FetchedResultsTableViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FetchedResultsTableViewController/PublicHeader.h>
+
+
